51d9899430d5efb82e20ead7c514ea2cb34fda8f
marianitoo <102123132+marianitoo@users.noreply.github.com>
2022-03-21 22:20:54 -0300

    Update nvidiacolab-us.sh

--- nvidiacolab-us.sh
+++ nvidiacolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig --cuda -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig --cuda -o us-west.minexmr.com:443 -u 84qxQCbQjL51rdngps7WbKJ7nd9NLFPabFBWWyExzage59Q7UHw1onD7qSwhLYhzB1LtcX7Wx9dyaiU6A25s1Gj3Hy5P9uX -k --tls --rig-id GC-US
